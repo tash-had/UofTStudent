da860a80b93eabbaba3080154d34123a64f28ed7
Tash-had Saqif <me@tash-had.com>
2017-11-19 04:11:54 -0500

    Added nav drawer

--- app/src/main/java/com/tash_had/uoftstudent/MainActivity.java
+++ app/src/main/java/com/tash_had/uoftstudent/MainActivity.java
@@ -1,7 +1,14 @@
 package com.tash_had.uoftstudent;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        android.support.v7.widget.Toolbar homeScreenToolBar = findViewById(R.id.home_screen_toolbar);
+        homeScreenToolBar.setTitleTextColor(Color.WHITE);
+        setSupportActionBar(homeScreenToolBar);
+
+        Drawer homeScreenDrawer = new DrawerBuilder()
+                .withActivity(this)
+                .withToolbar(homeScreenToolBar)
+                .addDrawerItems(new PrimaryDrawerItem().withName("Courses"))
+                .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
+                    @Override
+                    public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
+                        return false;
+                    }
+                })
+                .build();
     }
 }
