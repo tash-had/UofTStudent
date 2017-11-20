9a6ad8b88aa715a6b741ef2b7521d31c845cd7e4
Tash-had Saqif <me@tash-had.com>
2017-11-20 03:22:56 -0500

    Started work on home screen layout + course cards

--- app/src/main/java/com/tash_had/uoftstudent/MainActivity.java
+++ app/src/main/java/com/tash_had/uoftstudent/MainActivity.java
@@ -1,14 +1,21 @@
 package com.tash_had.uoftstudent;
 
 import android.graphics.Color;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import com.mikepenz.materialdrawer.AccountHeader;
+import com.mikepenz.materialdrawer.AccountHeaderBuilder;
 import com.mikepenz.materialdrawer.Drawer;
 import com.mikepenz.materialdrawer.DrawerBuilder;
 import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.ProfileDrawerItem;
+import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
 import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IProfile;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,16 +25,64 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         android.support.v7.widget.Toolbar homeScreenToolBar = findViewById(R.id.home_screen_toolbar);
-        homeScreenToolBar.setTitleTextColor(Color.WHITE);
-        setSupportActionBar(homeScreenToolBar);
 
-        Drawer homeScreenDrawer = new DrawerBuilder()
+        homeScreenToolBar.setTitleTextColor(Color.WHITE);
+
+        setSupportActionBar(homeScreenToolBar);
+        ActionBar actionBar = getSupportActionBar();
+
+        Drawer homeScreenDrawer = buildNavDrawer(homeScreenToolBar, buildAccountHeader(), buildDrawerItems());
+    }
+
+    private Drawer buildNavDrawer(Toolbar toolbar, AccountHeader accountHeader, IDrawerItem ... drawerItems){
+        return new DrawerBuilder()
                 .withActivity(this)
-                .withToolbar(homeScreenToolBar)
-                .addDrawerItems(new PrimaryDrawerItem().withName("Courses"))
+                .withToolbar(toolbar)
+                .addDrawerItems(drawerItems)
                 .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
                     @Override
                     public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
+                        return false;
+                    }
+                })
+                .withAccountHeader(buildAccountHeader())
+                .build();
+    }
+
+    private IDrawerItem[] buildDrawerItems(){
+        IDrawerItem home = new PrimaryDrawerItem().withName("Home")
+                .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
+            @Override
+            public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
+                return false;
+            }
+        });
+        IDrawerItem courses = new SecondaryDrawerItem().withName("Courses")
+                .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
+                    @Override
+                    public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
+                        return false;
+                    }
+                });
+        IDrawerItem settings = new SecondaryDrawerItem().withName("Settings")
+                .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
+                    @Override
+                    public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
+                        return false;
+                    }
+                });
+        return new IDrawerItem[]{home, courses, settings};
+
+    }
+
+    private AccountHeader buildAccountHeader(){
+        return new AccountHeaderBuilder()
+                .withActivity(this)
+                .withHeaderBackground(R.drawable.header)
+                .addProfiles(new ProfileDrawerItem().withName("Guest").withEmail("guest@utoronto.ca").withIcon(R.drawable.g_letter_icon))
+                .withOnAccountHeaderListener(new AccountHeader.OnAccountHeaderListener() {
+                    @Override
+                    public boolean onProfileChanged(View view, IProfile profile, boolean current) {
                         return false;
                     }
                 })
