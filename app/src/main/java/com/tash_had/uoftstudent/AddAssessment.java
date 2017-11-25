22917ff3cfddb53fcf10aefef86b6ec3c45a1c5c
Tash-had Saqif <me@tash-had.com>
2017-11-25 04:10:01 -0500

    Started work on Add Course screen

--- app/src/main/java/com/tash_had/uoftstudent/AddAssessment.java
+++ app/src/main/java/com/tash_had/uoftstudent/AddAssessment.java
@@ -1,17 +1,46 @@
 package com.tash_had.uoftstudent;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-/**
- * Created by tash-had on 2017-11-25.
- */
+import com.rengwuxian.materialedittext.MaterialEditText;
+import com.rengwuxian.materialedittext.validation.RegexpValidator;
+
 
 public class AddAssessment extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.form);
+
+        android.support.v7.widget.Toolbar homeScreenToolBar = findViewById(R.id.form_toolbar);
+        homeScreenToolBar.setTitle("");
+        homeScreenToolBar.setTitleTextColor(ResourcesCompat.getColor(getResources(), R.color.white, null));
+        homeScreenToolBar.setBackgroundColor(ResourcesCompat.getColor(getResources(), R.color.primary, null));
+
+        setSupportActionBar(homeScreenToolBar);
+        if (getSupportActionBar() != null){
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
     }
+
+
+//    private void initValidationEt() {
+//        final MaterialEditText validationEt = (MaterialEditText) findViewById(R.id.validationEt);
+//        validationEt.addValidator(new RegexpValidator("Only Integer Valid!", "\\d+"));
+//        final Button validateBt = (Button) findViewById(R.id.validateBt);
+//        validateBt.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                // validate
+//                validationEt.validate();
+//            }
+//        });
+//    }
 }
--- app/src/main/java/com/tash_had/uoftstudent/MainActivity.java
+++ app/src/main/java/com/tash_had/uoftstudent/MainActivity.java
@@ -38,11 +38,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         // Toolbar
         android.support.v7.widget.Toolbar homeScreenToolBar = findViewById(R.id.home_screen_toolbar);
         homeScreenToolBar.setTitle("Home");
         homeScreenToolBar.setTitleTextColor(ResourcesCompat.getColor(getResources(), R.color.white, null));
+        setSupportActionBar(homeScreenToolBar);
 
         // Set test student
         SessionData.sessionStudent = CodeTest.testStudent();
